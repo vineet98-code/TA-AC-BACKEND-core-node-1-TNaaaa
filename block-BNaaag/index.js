60a43dc3e51e8aa33a6d7c5ae56a678a67165bb0
Vineet <vineet98love@gmail.com>
2021-09-13 15:38:48 +0530

    update

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,20 +1,33 @@
-var fs = require('fs');
-
-console.log('excute me first');
-
-
-console.log('task1');
-setTimeout(() => {
-    console.log('excuted');
-    console.timeEnd('task1');
-
-}, 1000);
+var {readFile, readFileSync} = require('fs');
+// read file is synchronous 
 
 
-console.time('task2');
-fs.readFile('./file.md', 'utf8', (err, content) => {
+// Async version of read file
+readFile('./content.md', 'utf8', (err, content) => { // utf-8 return the actual content of the content.md file
     console.log(content);
     console.timeEnd('task2');
 })
 
-console.log('execute me last');
+// sync version of read file
+var result = readFileSync('./content.md', 'utf8');
+
+console.log(result);
+
+
+var buff1 = Buffer.alloc(10);
+buff1.write('Welcome to Buffer');
+console.log(buff1)
+// console.log('excute me first');
+
+
+// console.log('task1');
+// setTimeout(() => {
+//     console.log('excuted');
+//     console.timeEnd('task1');
+
+// }, 1000);
+
+
+// console.time('task2');
+
+// console.log('execute me last');
