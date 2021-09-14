d11fd26ca799739b57dc0e351fcaf0a8da4ebe8f
Vineet <vineet98love@gmail.com>
2021-09-14 14:37:21 +0530

    server

---
+++ block-BNaaal/server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+    res.end('Welcome');
+}
+
+server.listen(3000, () => {
+    console.log('server listening on port 3000');
+})
