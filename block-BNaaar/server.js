48a0bd519708c77d658b3b3e566613d55f5800e7
Vineet <vineet98love@gmail.com>
2021-09-15 14:18:42 +0530

    block-BNaaar

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -4,12 +4,22 @@ var server = http.createServer(handleRequest);
 
 function handleRequest(req, res){
     console.log(req.method, req.url);
-    res.statusCode = 201;
-    // res.setHeader('Content-Type', 'text/html');
+    if(req.method === 'GET' && req.url === '/'){
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Welcome to homepage');
+    } else if(req.method === 'GET' && req.url === '/about'){
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('<h2>this is all about NodeJs</h2>');
+    } else if(req.method === 'POST' && req.url === '/about'){
+        res.setHeader('Content-Type', 'application/json');
+        res.end('{message: this is a post request}');
+
+    }
+
+    // res.setHeader('Content-Type', 'text/html');    or  write this
     // res.writeHead(200, {'content-Type': 'text/html'});
-    res.end('<h2>Hello Campus</h2>');
 }
 
 server.listen(5000, () => {
-    console.log('server listening on port 4444');
+    console.log('server listening on port 5000');
 })
